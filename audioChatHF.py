6a62b771c594d5d731f6f6acb6b3b1e5571ee19a
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-09-18 12:09:59 +0800

    Update audioChatHF.py

--- audioChatHF.py
+++ audioChatHF.py
@@ -111,7 +111,7 @@ if audio_listen_cbox:
              recognizer.recognize_google(audio_data=audio_file)
              st.audio(audio, format="audio/mpeg") 
              #st.write(type(audio_file))
-             st.write("Recognizing your audio...wait a while to cheers!")
+             with st.spinner("Recognizing your audio...Please wait a while to Cheers!", key="audio_txt_spinner"):                  
              audio_txt_result = recognizer.recognize_google(audio_data=audio_file, language=input_language )
              st.write("基于您的输入语言"+input_language+"，识别您的输入为：\n\n"+audio_txt_result)
 #             st.write("---")       
@@ -129,7 +129,7 @@ ai_response_cbox = st.checkbox("查看AI助手回复", key="ai_cbox")
 if ai_response_cbox:
     user_query = audio_txt_result   
     if user_query !="" and not user_query.strip().isspace() and not user_query == "" and not user_query.strip() == "" and not user_query.isspace():         
-      with st.spinner("AI Thinking...Please wait a while to Cheers!"):   
+      with st.spinner("AI Thinking...Please wait a while to Cheers!", key="ai_spinner"):   
         initial_response=llm_chain.run(user_query)
         temp_ai_response_1=initial_response.partition('<|end|>\n<|user|>\n')[0]
         temp_ai_response_2=temp_ai_response_1.replace('<|end|>\n<|assistant|>\n', '') 
